beeed3a138b52fd400ca429b87c4581aea37dc3e
Axe-Usa <naimatropical@gmail.com>
2017-12-31 17:31:15 +0100

    Create AssemblyInfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TelegramTools")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SochiX")]
+[assembly: AssemblyProduct("TelegramTools")]
+[assembly: AssemblyCopyright("Copyright SochiX ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6387ac06-a237-486e-b314-739ae3e936b4")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
